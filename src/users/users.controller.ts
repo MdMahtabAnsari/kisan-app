04103f3cb038fa91075bfd5ba8b1a78ce43a2155
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2026-04-01 07:07:05 +0530

    feat: add AllowAnonymous decorator to isUserExists endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { IdentifierDto } from './dto/identifier.dto';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @AllowAnonymous()
   @Get('isAvailable/:identifier')
   async isUserExists(@Body() identifierDto: IdentifierDto) {
     return this.usersService.isUserExists(identifierDto.identifier);
